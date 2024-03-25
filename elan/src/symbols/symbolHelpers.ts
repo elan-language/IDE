f48a78e787676010792a3fec302c5f9114b12715
Stefano Cascarini <stef.cascarini@googlemail.com>
2024-03-25 17:31:37 +0000

    validate frame

--- elan/src/symbols/symbolHelpers.ts
+++ elan/src/symbols/symbolHelpers.ts
@@ -1,4 +1,5 @@
 import { FrameWithStatements } from "../frames/frame-with-statements";
+import { isFrame } from "../frames/helpers";
 import { Field } from "../frames/interfaces/field";
 import { Frame } from "../frames/interfaces/frame";
 import { ISymbol } from "./ISymbol";
@@ -9,17 +10,26 @@ export function isSymbol(s?: any): s is ISymbol {
 
 export function findSymbolInScope(id: string, field: Field): ISymbol | undefined {
     var holder = field.getHolder();
-    var parentOfHolder = holder.getParent();
+    if (isFrame(holder)) {
+        return findSymbolInFrameScope(id, holder);
+    }
+    return undefined;
+}
+
+export function findSymbolInFrameScope(id: string, frame: Frame): ISymbol | undefined {
+    var parentOfHolder = frame.getParent();
 
     if (parentOfHolder instanceof FrameWithStatements) {
         var fst = parentOfHolder.getFirstChild();
-        var range = parentOfHolder.getChildRange(fst, holder as Frame);
+        var range = parentOfHolder.getChildRange(fst, frame as Frame);
         if (range.length > 1) {
             range = range.slice(0, range.length - 1);
 
             for (var f of range) {
                 if (isSymbol(f)) {
-                    return f;
+                    if (f.symbolId === id) {
+                        return f;
+                    }
                 }
             }
         }
